3b9cd32f57949fdd7f2bf1be6913dd4d045372ec
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-07 23:27:19 +0000

    adds countOnly function

--- countOnly.js
+++ countOnly.js
@@ -3,3 +3,36 @@ const assertEqual = function(actual, expected) {
   else console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (item in results) {
+        results[item]++;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  console.log(results);
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Kariman": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
