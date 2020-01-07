f61a02280a16c3cf9211c79d28e0dfd136d35e6c
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-07 23:42:58 +0000

    removes tests, removes non-letter characters

--- countLetters.js
+++ countLetters.js
@@ -2,3 +2,10 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   else console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
+
+const countLetters = sentence => {
+  sentenceLetters = sentence.replace(/[^a-zA-Z]/g, "");
+  for (letter of sentenceLetters){
+    console.log(letter);
+  }
+}
