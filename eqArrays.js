94dc0967d42b438cdefb55db3f76608c9278162d
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-07 20:07:24 +0000

    adds eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  else console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length)
+    return false;
+  let equalArrays = true;
+  arr1.forEach((el, i) => {
+    if (el !== arr2[i])
+      equalArrays = false;
+  });
+  return equalArrays;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3,4]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
+assertEqual(eqArrays([''], []), false);
+assertEqual(eqArrays([], []), true);
