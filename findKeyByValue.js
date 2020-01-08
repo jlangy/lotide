2ef8748294133afa3b020786bbb07af3aceea802
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-08 00:34:25 +0000

    adds more tests

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -5,18 +5,31 @@ const assertEqual = function(actual, expected) {
 
 const findKeyByValue = (object, value) => {
   const keys = Object.keys(object);
-  for(let i = 0; i < keys.length; i++){
+  for (let i = 0; i < keys.length; i++) {
     if (object[keys[i]] === value)
       return keys[i];
   }
   return undefined;
-}
-
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
 };
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  news: "The Expanse"
+};
+
+const alphaNum = {
+  a:1,
+  b:1,
+  c:4,
+  e:2,
+  g:4
+};
+
+const empty = {};
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(alphaNum, 4), "c");
+assertEqual(findKeyByValue(empty, "That '70s Show"), undefined);
