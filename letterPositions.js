34ebbda9dc78d40aa20aacd02a6d21cc7f493651
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-08 00:06:43 +0000

    adds letterpositions function

--- letterPositions.js
+++ letterPositions.js
@@ -16,7 +16,15 @@ const eqArrays = (arr1, arr2) => {
 
 const letterPositions = sentence => {
   const results = {};
-
+  lowerCaseSentence = sentence.toLowerCase();
+  const letters = lowerCaseSentence.split('');
+  letters.forEach((letter,i) => {
+    if(letter in results){
+      results[letter].push(i);
+    } else {
+      results[letter] = [i];
+    }
+  });
   return results;
 }
 
