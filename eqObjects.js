eab80008cc5608fcdcc66ea0295ac8109b5ff8f8
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-08 01:27:06 +0000

    adds array test to eqObjects function

--- eqObjects.js
+++ eqObjects.js
@@ -3,13 +3,43 @@ const assertEqual = function(actual, expected) {
   else console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-const eqObjects = (obj1, obj2) => {
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length)
+    return false;
+  let equalArrays = true;
+  arr1.forEach((el, i) => {
+    if (el !== arr2[i])
+      equalArrays = false;
+  });
+  return equalArrays;
+};
 
+const eqObjects = (obj1, obj2) => {
+  const keys = Object.keys(obj1);
+  if (keys.length !== Object.keys(obj2).length)
+    return false;
+  for(let i = 0; i < keys.length; i++){
+    const key = keys[i];
+    if(!(key in obj2)){
+      return false;
+    } if (obj1[key] instanceof Array) {
+      return eqArrays(obj1[key], obj2[key]);
+    } 
+  };
+  return true;
 }
 
 const ab = {a: '1', b: '2'};
 const ba = {b: '2', a: '1'};
-const abc = {a: '1', b: '2', c: '3'}
+const abc = {a: '1', b: '2', c: '3'};
+
+const cd = {c: '1', d:["2", 3]};
+const dc = {d:["2", 3], c: '1'};
+
+const cd2 = {c:'1', d: ['2',3,4]};
 
 assertEqual(eqObjects(ab,ba), true);
 assertEqual(eqObjects(abc,ba), false);
+
+assertEqual(eqObjects(cd,cd2), false);
+
