5608ff19fd16b5fcbe48a5b3c4a431043ab0eefd
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-08 00:42:20 +0000

    sets up eqObjects.js, adds tests

---
+++ eqObjects.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  else console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqObjects = (obj1, obj2) => {
+
+}
+
+const ab = {a: '1', b: '2'};
+const ba = {b: '2', a: '1'};
+const abc = {a: '1', b: '2', c: '3'}
+
+assertEqual(eqObjects(ab,ba), true);
+assertEqual(eqObjects(abc,ba), false);
