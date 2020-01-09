83d0011befb0c5443ad151c64d96d99895bd49fd
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-09 17:42:58 +0000

    Adds recursive check to eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -6,16 +6,22 @@ const assertEqual = function(actual, expected) {
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length)
     return false;
-  let equalArrays = true;
-  arr1.forEach((el, i) => {
-    if (el !== arr2[i])
-      equalArrays = false;
-  });
-  return equalArrays;
+  for(let i = 0; i < arr2.length; i++){
+    if(Array.isArray(arr1[i]) || Array.isArray(arr2[i])){
+      return eqArrays(arr1[i], arr2[i]);
+    }
+    if (arr1[i] !== arr2[i])
+      return false;
+  };
+  return true;
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, [3]], [1, 2, [3]]), true);
+assertEqual(eqArrays([1, 2, [3,4,[5,6,7,[8,9]]]], [1, 2, [3,4,[5,6,7,[8,9]]]]), true);
+assertEqual(eqArrays([1, 2, [3,4,[5,6,7,[8,10]]]], [1, 2, [3,4,[5,6,7,[8,9]]]]), false);
+assertEqual(eqArrays([1, 2, [3]], [1, 2, [3]]), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3,4]), false);
 assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
 assertEqual(eqArrays([''], []), false);
 assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays([[]], []), false);
