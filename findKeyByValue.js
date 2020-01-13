75895bfddddbbccfd631f72a17fafefec5006199
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-13 21:49:18 +0000

    Adds mocha/chai testing for findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,8 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  else console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const findKeyByValue = (object, value) => {
   const keys = Object.keys(object);
   for (let i = 0; i < keys.length; i++) {
@@ -11,27 +6,5 @@ const findKeyByValue = (object, value) => {
   }
   return undefined;
 };
-
-const bestTVShowsByGenre = {
-  sciFi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire",
-  news: "The Expanse"
-};
-
-const alphaNum = {
-  a:1,
-  b:1,
-  c:4,
-  e:2,
-  g:4
-};
-
-const empty = {};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(alphaNum, 4), "c");
-assertEqual(findKeyByValue(empty, "That '70s Show"), undefined);
 
 module.exports = findKeyByValue;
---
+++ test/findKeyByValueTest.js
@@ -0,0 +1,28 @@
+const assert = require('chai').assert;
+const findKeyByValue = require('../findKeyByValue');
+
+const alphaNum = {
+  a:1,
+  b:1,
+  c:4,
+  e:2,
+  g:4
+};
+
+describe('findKeyByValue', () => {
+  it('returns undefined when value not present', () => {
+    assert.strictEqual(findKeyByValue(alphaNum, 5), undefined);
+  });
+  it('returns the correct value count', () => {
+    assert.strictEqual(findKeyByValue(alphaNum, 1), 'a');
+    assert.strictEqual(findKeyByValue(alphaNum, 2), 'e');
+    assert.strictEqual(findKeyByValue(alphaNum, 4), 'c');
+  });
+});
+
+
+
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// assertEqual(findKeyByValue(alphaNum, 4), "c");
+// assertEqual(findKeyByValue(empty, "That '70s Show"), undefined);
